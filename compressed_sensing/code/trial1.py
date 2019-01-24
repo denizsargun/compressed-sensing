ff0bb054afeda2a6d161ea869da04c3e94d0eee5
denizsargun <denizsargun@gmail.com>
2019-01-24 15:54:31 -0500

    example

---
+++ compressed_sensing/code/trial1.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import scipy.optimize as spopt
+import scipy.fftpack as spfft
+import scipy.ndimage as spimg
+import cvxpy as cvx
+
+n = 5000
+t = np.linspace(0, 1/8, n)
+y = np.sin(1394 * np.pi * t) + np.sin(3266 * np.pi * t)
+yt = spfft.dct(y, norm='ortho')
