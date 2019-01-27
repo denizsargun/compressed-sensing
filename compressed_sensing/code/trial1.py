4e32a840e35af7d0c9014b5d031b79d8d1308c05
denizsargun <denizsargun@gmail.com>
2019-01-27 16:38:27 -0500

    code for compressing image

--- compressed_sensing/code/trial1.py
+++ compressed_sensing/code/trial1.py
@@ -6,7 +6,18 @@ import scipy.fftpack as spfft
 import scipy.ndimage as spimg
 import cvxpy as cvx
 
-n = 5000
-t = np.linspace(0, 1/8, n)
-y = np.sin(1394 * np.pi * t) + np.sin(3266 * np.pi * t)
-yt = spfft.dct(y, norm='ortho')
+# n = 5000
+# t = np.linspace(0, 1/8, n)
+# y = np.sin(1394 * np.pi * t) + np.sin(3266 * np.pi * t)
+# yt = spfft.dct(y, norm='ortho')
+
+def dct2(x):
+    return spfft.dct(spfft.dct(x.T, norm='ortho', axis=0).T, norm='ortho', axis=0)
+
+def idct2(x):
+    return spfft.idct(spfft.idct(x.T, norm='ortho', axis=0).T, norm='ortho', axis=0)
+
+# read original image and downsize for speed
+Xorig = spimg.imread('escher_waterfall.jpeg', flatten=True, mode='L') # read in grayscale
+X = spimg.zoom(Xorig, 0.04)
+ny,nx = X.shape
