507e9fb4cd8b1b1cfde9836c2a2fbf9c047db8e9
denizsargun <denizsargun@gmail.com>
2019-01-27 19:04:43 -0500

    image compression example code

--- compressed_sensing/code/trial1.py
+++ compressed_sensing/code/trial1.py
@@ -1,3 +1,4 @@
+# http://www.pyrunner.com/weblog/2016/05/26/compressed-sensing-python/
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
@@ -18,6 +19,32 @@ def idct2(x):
     return spfft.idct(spfft.idct(x.T, norm='ortho', axis=0).T, norm='ortho', axis=0)
 
 # read original image and downsize for speed
-Xorig = spimg.imread('escher_waterfall.jpeg', flatten=True, mode='L') # read in grayscale
+Xorig = spimg.imread('escher_waterfall.jpg', flatten=True, mode='L') # read in grayscale
+plt.imshow(Xorig)
+
 X = spimg.zoom(Xorig, 0.04)
+plt.figure()
+plt.imshow(X)
+
 ny,nx = X.shape
+k = round(nx * ny * 0.5) # 50% sample
+ri = np.random.choice(nx * ny, k, replace=False) # random sample of indices
+b = X.T.flat[ri]
+b = np.expand_dims(b, axis=1)
+
+# create dct matrix operator using kron (memory errors for large ny*nx)
+A = np.kron(
+    spfft.idct(np.identity(nx), norm='ortho', axis=0),
+    spfft.idct(np.identity(ny), norm='ortho', axis=0)
+    )
+A = A[ri,:] # same as phi times kron
+
+# do L1 optimization
+vx = cvx.Variable(nx * ny)
+objective = cvx.Minimize(cvx.norm(vx, 1))
+constraints = [A*vx == b]
+prob = cvx.Problem(objective, constraints)
+result = prob.solve(verbose=True)
+Xat2 = np.array(vx.value).squeeze()
+plt.imshow(Xat2)
+plt.show()
